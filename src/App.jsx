aecd251bdbbd32a046c8edc4233e24ed5f7a8a08
dehas99 <dehas99@gmail.com>
2023-01-02 11:45:50 +0100

    Layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello Deeq</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div>
+      <h1>Im a footer</h1>
+      <h2>Yeah boi</h2>
+    </div>
+  );
+}
+
+export default Footer;
